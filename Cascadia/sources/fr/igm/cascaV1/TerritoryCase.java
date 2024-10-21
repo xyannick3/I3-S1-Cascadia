73a73abb7f32eabb797da13526fb1e46ba0b84ef
xyannick3 <xyannick3@gmail.com>
2024-10-21 17:59:11 +0200

    fix TerritoryCase
    fix de la classe qui gère les cases

--- Cascadia/sources/fr/igm/cascaV1/TerritoryCase.java
+++ Cascadia/sources/fr/igm/cascaV1/TerritoryCase.java
@@ -1,4 +1,4 @@
-package fr.igm.cascabeta;
+package fr.igm.cascaV1;
 
 import java.util.List;
 import java.util.Objects;
@@ -7,18 +7,33 @@ import fr.igm.cascabeta.enums.TerritoryType;
 import fr.igm.cascabeta.enums.WildlifeToken;
 
 public class TerritoryCase {
-  private TerritoryType territory;
+  private TerritoryType[] sides;
   private WildlifeToken wildlife;
   private List<WildlifeToken> allowedWildlife;
-  //Constructor assigns to the case a territory and a list of allowed wildlife on the token.
-  public TerritoryCase(TerritoryType territory, List<WildlifeToken> allowedWildlife) {
-    Objects.requireNonNull(territory);
+  //Constructor
+  public TerritoryCase(TerritoryType territory1,TerritoryType territory2, List<WildlifeToken> allowedWildlife) {
+    Objects.requireNonNull(territory1);
+    Objects.requireNonNull(territory2);
     Objects.requireNonNull(allowedWildlife);
-    this.territory = territory;
+    this.sides = new TerritoryType[] {territory1,territory1,territory2,territory2};
     this.allowedWildlife = allowedWildlife;
     this.wildlife = null;
   }
-
+  //rotate the case clockwise by 90 degrees
+  public void rotateClockwise() {
+    var buffer = sides[3];
+    sides[3] = sides[2];
+    sides[1] = sides[0];
+    sides[0] = buffer;
+  }
+  //same as above but counterclockwise
+  public void rotateCounterClockwise() {
+    var buffer = sides[0];
+    sides[0] = sides[1];
+    sides[1] = sides[2];
+    sides[2] = sides[3];
+    sides[3] = buffer;
+  }
   //adds a wildlife to the case
   public boolean addWildlife(WildlifeToken token) {
     Objects.requireNonNull(token);
@@ -29,8 +44,9 @@ public class TerritoryCase {
     return false;
   }
   //returns the territory type of the designated side
-  public TerritoryType getTerritory() {
-    return territory;
+  public TerritoryType getTerritoryOnSide(int index) {
+    Objects.requireNonNull(index);
+    return sides[index %4];
   }
   //returns the wildlife assigned to the case
   public WildlifeToken getWildlife() {
@@ -43,8 +59,14 @@ public class TerritoryCase {
   //override of the to string function
   public String toString() {
     var result = new StringBuilder();
-    result.append("Territory : ");
-    result.append(territory);
+    result.append("North : ");
+    result.append(sides[0]);
+    result.append(", East: ");
+    result.append(sides[1]);
+    result.append(", South :");
+    result.append(sides[2]);
+    result.append(",West: ");
+    result.append(sides[3]);
     result.append(". Wildlife");
     result.append((wildlife != null ? wildlife : "None"));
     result.append("Allowed animals");
--- Cascadia/sources/fr/igm/cascabeta/TerritoryPile.java
+++ Cascadia/sources/fr/igm/cascabeta/TerritoryPile.java
@@ -1,5 +1,4 @@
 package fr.igm.cascabeta;
-import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
@@ -9,6 +8,7 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.Random;
+import java.nio.file.*;
 
 import fr.igm.cascabeta.enums.TerritoryType;
 import fr.igm.cascabeta.enums.WildlifeToken;
@@ -18,40 +18,7 @@ public class TerritoryPile {
   private  List<TerritoryCase> pile;
   //n est le nombre de joueurs
   public TerritoryPile(String csvFilePath,int n) {
-    Objects.requireNonNull(n);
-    pile = new ArrayList<>();
-    try (BufferedReader br = new BufferedReader(new FileReader(csvFilePath))) {
-      String line;
-      boolean isFirstLine = true; // saute entête
-      while ((line = br.readLine()) != null) {
-          if (isFirstLine) {
-              isFirstLine = false;
-              continue; 
-          }
-          String[] values = line.split(",");
-          TerritoryType t1 = TerritoryType.valueOf(values[0].trim());
-          TerritoryType t2 = TerritoryType.valueOf(values[1].trim());
-
-          // Parse Wildlife (columns 3, 4, 5), ignore empty strings
-          List<WildlifeToken> allowedWildlife = new ArrayList<>();
-          if (!values[2].isEmpty()) allowedWildlife.add(WildlifeToken.valueOf(values[2].trim()));
-          if (values.length >3 && (!values[3].isEmpty())) allowedWildlife.add(WildlifeToken.valueOf(values[3].trim()));
-          if (values.length >4 && (!values[4].isEmpty())) allowedWildlife.add(WildlifeToken.valueOf(values[4].trim()));
-
-          // Create a new Case object
-          var newCase = new TerritoryCase(t1, t2, allowedWildlife);
-          System.out.println(newCase);
-          pile.add(newCase);
-      }
-      melanger();
-      //TODO ajuster le nombre de token territoire proportionellement à n le nombre de joueurs
-    } catch (FileNotFoundException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    } catch (IOException e) {
-      // TODO Auto-generated catch block
-      e.printStackTrace();
-    }
+    
   }
   public void melanger() {
     Collections.shuffle(pile, new Random());
